6a4625dc9a4ee86f457f150f860e7f5172a687fe
Malgorzata Sissing <kropka@Malgorzatas-MacBook-Pro.local>
2023-07-30 08:20:45 +0100

    feat: added nx,angular and strapi

--- apps/web-app/src/app/app.component.ts
+++ apps/web-app/src/app/app.component.ts
@@ -1,14 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { NxWelcomeComponent } from './nx-welcome.component';
+import { HttpClient } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule],
+  imports: [CommonModule, NxWelcomeComponent, RouterModule],
   selector: 'org-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
+  httpClient = inject(HttpClient);
+  data: any;
+
+  ngOnInit(): void {
+    this.httpClient
+      .get('http://localhost:1337/api/articles')
+      .subscribe((res) => {
+        console.log('res', res);
+        this.data = res;
+      });
+  }
+
   title = 'web-app';
 }
--- apps/web-app/src/app/app.config.ts
+++ apps/web-app/src/app/app.config.ts
@@ -4,7 +4,13 @@ import {
   withEnabledBlockingInitialNavigation,
 } from '@angular/router';
 import { appRoutes } from './app.routes';
+import { provideHttpClient, withNoXsrfProtection } from '@angular/common/http';
+import { provideClientHydration } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(appRoutes, withEnabledBlockingInitialNavigation())],
+  providers: [
+    provideRouter(appRoutes, withEnabledBlockingInitialNavigation()),
+    provideClientHydration(),
+    provideHttpClient(withNoXsrfProtection()),
+  ],
 };
